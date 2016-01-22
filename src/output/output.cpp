f4e6eaafc200af0e32e41137fb28251e5fc502a2
Dendi Suhubdy <dendi@suhubdy.com>
2016-01-21 23:16:22 -0500

    class structure

---
+++ src/output/output.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: output.cpp
+	> Author: Dendi Suhubdy 
+	> Mail: dasuhubd@ncsu.edu
+	> Created Time: Thu Jan 21 23:15:53 2016
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
